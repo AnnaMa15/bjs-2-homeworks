d726ac75bd7aa07382aec392c4806d578fc12dd5
AnnaMa15 <122748925+AnnaMa15@users.noreply.github.com>
2023-06-18 15:58:01 +0300

    Add files via upload

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,120 @@
-function getArrayParams(...arr) {
+"use strict";
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  let avg = +(sum / arr.length).toFixed(2);
+
+  return {
+    max: max,
+    min: min,
+    avg: avg
+  };
 }
 
-function summElementsWorker(...arr) {
+console.log(getArrayParams(-99, 99, 10))
 
+
+
+
+'use strict';
+
+function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEven = 0;
+  let sumOdd = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEven += arr[i];
+    } else {
+      sumOdd += arr[i];
+    }
+  }
+  return sumEven - sumOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEven = 0;
+  let countEven = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEven += arr[i];
+      countEven++;
+    }
+  }
+  if (countEven === 0) {
+    return 0;
+  }
+  return +(sumEven / countEven).toFixed(2);
 }
 
-function makeWork (arrOfArr, func) {
+console.log(summElementsWorker(10, 10, 11, 20, 10));
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
+console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
 
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const workerResult = func(...arrOfArr[i]);
+    if (workerResult > maxWorkerResult) {
+      maxWorkerResult = workerResult;
+    }
+  }
+  return maxWorkerResult;
 }
+
+const arr = [
+  [10, 10, 11, 20, 10],
+  [67, 10, 2, 39, 88],
+  [72, 75, 51, 87, 43],
+  [30, 41, 55, 96, 62]
+];
+console.log(makeWork(arr, summElementsWorker));
+console.log(makeWork(arr, differenceMaxMinWorker));
+console.log(makeWork(arr, differenceEvenOddWorker));
+console.log(makeWork(arr, averageEvenElementsWorker));
